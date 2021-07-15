508d6676868835b3ac8786d5ec74142ec7da23a5
Mullkaw <admiralmemes@gmail.com>
2021-07-15 06:59:45 -0400

    Change output file name

--- program.py
+++ program.py
@@ -14,17 +14,36 @@ if not spawn.find_executable('wbmgt'):
     quit()
 
 for arg in sys.argv[1:]:
-    # decode the bmg and create the bmg object
-    spc.run(f"wbmgt decode --single-line --dest output.txt {arg}").check_returncode()
-    wii_bmg = Bmg('output.txt')
+    # the argument with no file extension
+    arg_no_ext = arg[:arg.rfind('.')] if arg.rfind('.') != -1 else arg
 
-    # wii_bmg.messages = wii_bmg.messages[77:83]
+    # the file extension (including the dot)
+    ext = arg[arg.rfind('.'):] if arg.rfind('.') != -1 else ""
+
+    if os.path.isfile(f"{arg_no_ext}-translated.bmg"):
+        err = f"{arg_no_ext}-translated.bmg is already in this directory.\n" + \
+            "Move this file before running the program.\n"
+
+        sys.stderr.write(err)
+        continue
+
+    # decode the bmg and create the bmg object
+    # if it's a .txt file then we assume it is a BMG text file and dont decode
+    if 'txt' not in ext:
+        # https://szs.wiimm.de/wbmgt/cmd-decode.html
+        command = f"wbmgt decode --single-line --dest {arg_no_ext}.txt {arg}"
+        spc.run(command).check_returncode()
+        print()
+
+    wii_bmg = Bmg(f"{arg_no_ext}.txt")
 
     print(f"Translating {arg}")
+    print()
 
     config = ConfigParser()
     config.read('config.ini')
 
+    # config file has one section so we receive this one section
     parameters = config[config.sections()[0]]
 
     source_name = languages.get(alpha2=parameters['source_language']).name \
@@ -35,6 +54,7 @@ for arg in sys.argv[1:]:
 
     print(f"Translating from: {source_name}")
     print(f"to: {', '.join(target_names)}")
+    print()
 
     lines = [message.text for message in wii_bmg.messages]
 
@@ -43,15 +63,22 @@ for arg in sys.argv[1:]:
     for i, line in enumerate(lines):
         wii_bmg.messages[i].text = line 
 
-    with open('output-translated.txt', 'w', encoding='utf-8') as fo:
+    with open(f"{arg_no_ext}-translated.txt", 'w', encoding='utf-8') as fo:
         fo.write(str(wii_bmg))
 
-    spc.run(f"wbmgt encode output-translated.txt").check_returncode()
+    # https://szs.wiimm.de/wbmgt/cmd-encode.html
+    command = f"wbmgt encode {arg_no_ext}-translated.txt"
+    spc.run(command).check_returncode()
+    print()
 
     try:
-        os.remove('output.txt')
-        os.remove('output-translated.txt')
+        if 'txt' not in ext:
+            os.remove(f"{arg_no_ext}.txt")
+        
+        os.remove(f"{arg_no_ext}-translated.txt")
     except FileNotFoundError:
         pass
 
-    print("All done!")
+    print(f"Finished Translating {arg} !")
+    print(f"Output file is {arg_no_ext}-translated{ext}")
+    print()
